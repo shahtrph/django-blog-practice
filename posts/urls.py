219477b0cb8783c6de015709529bd371f3c97207
megasar <shahlearning5@gmail.com>
2026-03-14 11:17:51 +0500

    home and blogposts page added

--- posts/urls.py
+++ posts/urls.py
@@ -1,7 +1,11 @@
 
 from django.contrib import admin
 from django.urls import path
+from posts import views as views
 
 urlpatterns = [
+  path('',views.home,name='home'),
+  path('blogposts',views.blogposts,name='blogposts')
+
     
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
-# Create your views here.
+def home(request):
+  title="this is my title"
+  context={'title':title}
+  return render(request,'posts/home.html',context)
+
+
+
+def blogposts(request):
+  context={}
+  return render(request,'posts/blogposts.html',context)
+
+
